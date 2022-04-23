08f7d42249f5198b8f0e9286d471bfbb2a7c781e
Kevin West <kevinwestnyc@gmail.com>
2022-04-23 11:33:39 -0400

    tighten frontend

--- interview-challenge/src/components/ContentArea.js
+++ interview-challenge/src/components/ContentArea.js
@@ -1,12 +1,14 @@
-import React, {useState} from 'react'
-import GalleryDisplay from './GalleryDisplay'
-import DetailView from './DetailView'
+import React, { useState } from "react";
+import GalleryDisplay from "./GalleryDisplay";
+import DetailView from "./DetailView";
 
 export default function ContentArea() {
   return (
     <div className="content-area">
-        {/* <GalleryDisplay/> */}
-        <DetailView />
+      <div className="content-pages">
+        <GalleryDisplay/>
+        {/* <DetailView /> */}
+      </div>
     </div>
-  )
+  );
 }
