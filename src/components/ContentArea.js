60a530bf33fdfc57669a0fdabf4a0bb965f51a2c
Kevin West <kevinwestnyc@gmail.com>
2022-04-27 19:45:18 -0400

    added API to add names to unnamed random colors

--- src/components/ContentArea.js
+++ src/components/ContentArea.js
@@ -4,37 +4,47 @@ import DetailView from "./DetailView";
 
 export default function ContentArea({ primaryColor, randomButtonClicked }) {
   const [mainColor, setMainColor] = useState("");
-  const [mainColorName, setMainColorName] = useState("No Name")
+  const [mainColorName, setMainColorName] = useState("No Name");
 
   const getRandomColor = () => {
     const randomColor =
       "#" + (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6);
     setMainColor(randomColor);
+    getColorName(randomColor);
   };
 
-  const getColorName = () => {
-    
-  }
+  const getColorName = async (name) => {
+    let response = await fetch(
+      `https://www.thecolorapi.com/id?hex=${name.replace("#", "")}`
+    );
+    let jsonData = await response.json();
+    setMainColorName(jsonData.name.value);
+  };
 
   useEffect(() => {
-      setMainColor(primaryColor);
-  },[primaryColor])
+    setMainColor(primaryColor);
+  }, [primaryColor]);
 
   useEffect(() => {
-      randomButtonClicked > 0 &&
-    getRandomColor();
+    randomButtonClicked > 0 && getRandomColor();
   }, [randomButtonClicked]);
 
   return (
     <div className="content-area">
       <div className="content-pages">
         {mainColor ? (
-          <DetailView mainColor={mainColor} mainColorName={mainColorName} onClear={() => setMainColor("")} onColorChange={(color) => setMainColor(color)}/>
+          <DetailView
+            mainColor={mainColor}
+            mainColorName={mainColorName}
+            onClear={() => setMainColor("")}
+          />
         ) : (
-          <GalleryDisplay onColorChange={(color, name) => {
-            setMainColor(color)
-            setMainColorName(name)
-          }} />
+          <GalleryDisplay
+            onColorChange={(color, name) => {
+              setMainColor(color);
+              setMainColorName(name);
+            }}
+          />
         )}
       </div>
     </div>
